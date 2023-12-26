27be89adf4b777bfc603a2048975d17b41375651
pongsit-po <pongsit.po@outlook.com>
2023-12-26 16:05:06 +0700

    update v1.6  - add function to support webapp v6.4

--- dboperations.js
+++ dboperations.js
@@ -119,8 +119,27 @@ async function getTopicList() {
     console.log("connect complete");
     const result = await pool
       .request()
-      .query("SELECT * FROM trs_topic WHERE isactive = 1");
+      .query(
+        "SELECT * FROM trs_topic WHERE isactive = 1 AND GETDATE() > start_sdate AND GETDATE() < end_sdate ORDER BY id DESC"
+      );
     console.log("getTopicList complete");
+    console.log("====================");
+    return result.recordsets[0];
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function getAllTopicList() {
+  try {
+    console.log("getAllTopicList call try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    const result = await pool
+      .request()
+      .query("SELECT * FROM trs_topic ORDER BY id DESC");
+    console.log("getAllTopicList complete");
     console.log("====================");
     return result.recordsets[0];
   } catch (error) {
@@ -140,7 +159,32 @@ async function getSubTopicList() {
         "SELECT trs_topic_sub.*" +
           ", trs_topic_sub.id AS sub_id" +
           ", trs_topic.name AS topic_name" +
-          ", trs_topic.isactive" +
+          ", trs_topic.isactive AS topic_isactive" +
+          " FROM trs_topic_sub" +
+          " LEFT JOIN trs_topic ON trs_topic.id = trs_topic_sub.topic_id" +
+          " WHERE trs_topic_sub.isactive = 1"
+      );
+    console.log("getSubTopicList complete");
+    console.log("====================");
+    return result.recordsets[0];
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function getAllSubTopicList() {
+  try {
+    console.log("getSubTopicList call try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    const result = await pool
+      .request()
+      .query(
+        "SELECT trs_topic_sub.*" +
+          ", trs_topic_sub.id AS sub_id" +
+          ", trs_topic.name AS topic_name" +
+          ", trs_topic.isactive AS topic_isactive" +
           " FROM trs_topic_sub" +
           " LEFT JOIN trs_topic ON trs_topic.id = trs_topic_sub.topic_id"
       );
@@ -176,7 +220,8 @@ async function getAttd(psn_id) {
           " LEFT JOIN trs_topic_sub ON trs_topic_sub.id = trs_attd_list.sub_id" +
           " AND trs_topic_sub.topic_id = trs_attd_list.topic_id" +
           " LEFT JOIN trs_topic ON trs_topic.id = trs_attd_list.topic_id" +
-          " WHERE trs_attd_list.psn_id = @psn_id"
+          " WHERE trs_attd_list.psn_id = @psn_id" +
+          " ORDER BY trs_topic_sub.topic_id DESC"
       );
     console.log("attdChk complete");
     console.log("====================");
@@ -305,15 +350,6 @@ async function getExcelReport(topic_id) {
   }
 }
 
-async function getVersion() {
-  try {
-    return process.env.version;
-  } catch (error) {
-    console.error(error);
-    return { status: "error", message: error.message };
-  }
-}
-
 async function voteCheck(id) {
   try {
     console.log("voteCheck call try connect to server");
@@ -352,16 +388,187 @@ async function vote(data) {
   }
 }
 
+async function addTopic(data, topicId, img) {
+  try {
+    console.log("addTopic call, try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+
+    await pool
+      .request()
+      .input("id", sql.VarChar, topicId)
+      .input("name", sql.VarChar, data.name)
+      .input("start_sdate", sql.SmallDateTime, data.start_sdate)
+      .input("end_sdate", sql.SmallDateTime, data.end_sdate)
+      .input("img", sql.Bit, parseInt(img))
+      .input("remark", sql.VarChar, data.remark)
+      .input("create_by", sql.VarChar, data.create_by)
+      .query(
+        "INSERT INTO trs_topic" +
+          " (id, name, isactive, start_sdate, end_sdate, img, remark, create_by, create_date, last_edit_by, last_edit_date)" +
+          " VALUES (@id, @name, 1, @start_sdate, @end_sdate, @img, @remark, @create_by, GETDATE(), '', '')"
+      );
+
+    console.log("addTopic complete");
+    console.log("====================");
+    return { status: "ok", message: "เพิ่มข้อมูลลงระบบเรียบร้อยแล้ว" };
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function updateTopic(data, img) {
+  try {
+    console.log("updateTopic id : " + data.id + " call, try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    await pool
+      .request()
+      .input("id", sql.VarChar, data.id)
+      .input("name", sql.VarChar, data.name)
+      .input("start_sdate", sql.SmallDateTime, data.start_sdate)
+      .input("end_sdate", sql.SmallDateTime, data.end_sdate)
+      .input("img", sql.Bit, img ? 1 : 0)
+      .input("isactive", sql.Bit, parseInt(data.isactive))
+      .input("remark", sql.VarChar, data.remark)
+      .input("last_edit_by", sql.VarChar, data.last_edit_by)
+      .query(
+        "UPDATE trs_topic" +
+          " SET name = @name" +
+          ", start_sdate = @start_sdate" +
+          ", end_sdate = @end_sdate" +
+          ", img = @img" +
+          ", isactive = @isactive" +
+          ", remark = @remark" +
+          ", last_edit_by = @last_edit_by" +
+          ", last_edit_date = GETDATE()" +
+          " WHERE id = @id"
+      );
+
+    console.log("updateTopic complete");
+    console.log("====================");
+    return { status: "ok", message: "อัพเดทข้อมูลในระบบเรียบร้อยแล้ว" };
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function genNewSubTopicId(topic_id) {
+  console.log("generate new sub id");
+  let pool = await sql.connect(config);
+  const lastId = await pool
+    .request()
+    .input("topic_id", sql.VarChar, topic_id)
+    .query(
+      "SELECT TOP (1) id FROM trs_topic_sub WHERE topic_id = @topic_id ORDER BY id DESC"
+    );
+  if (lastId.recordset.length !== 0) {
+    const newId = lastId.recordset[0].id.slice(1);
+    return `S${String(parseInt(newId) + 1).padStart(2, "0")}`;
+  } else {
+    return "S01";
+  }
+}
+
+async function addSubTopic(data) {
+  try {
+    console.log(data);
+    console.log("addSubTopic call, try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    const id = await genNewSubTopicId(data.topic_id);
+    await pool
+      .request()
+      .input("topic_id", sql.VarChar, data.topic_id)
+      .input("id", sql.VarChar, id)
+      .input("name", sql.VarChar, data.name)
+      .input("lmt", sql.Int, data.lmt)
+      .input("start_date", sql.SmallDateTime, data.start_date)
+      .input("end_date", sql.SmallDateTime, data.end_date)
+      .input("remark", sql.VarChar, data.remark)
+      .input("create_by", sql.VarChar, data.create_by)
+      .query(
+        "INSERT INTO trs_topic_sub" +
+          " (topic_id, id, name, attd, lmt, start_date, end_date, isactive, remark, create_by, create_date, last_edit_by, last_edit_date)" +
+          " VALUES (@topic_id, @id, @name, 0, @lmt, @start_date, @end_date, 1, @remark, @create_by, GETDATE(), '', '')"
+      );
+
+    console.log("addSubTopic complete");
+    console.log("====================");
+    return { status: "ok", message: "เพิ่มข้อมูลลงระบบเรียบร้อยแล้ว" };
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function updateSubTopic(data) {
+  try {
+    console.log(
+      "updateSubTopic topic_id : " +
+        data.topic_id +
+        " id : " +
+        data.id +
+        " call, try connect to server"
+    );
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    await pool
+      .request()
+      .input("topic_id", sql.VarChar, data.topic_id)
+      .input("id", sql.VarChar, data.id)
+      .input("name", sql.VarChar, data.name)
+      .input("start_date", sql.SmallDateTime, data.start_date)
+      .input("end_date", sql.SmallDateTime, data.end_date)
+      .input("isactive", sql.Bit, parseInt(data.isactive))
+      .input("last_edit_by", sql.VarChar, data.last_edit_by)
+      .query(
+        "UPDATE trs_topic_sub" +
+          " SET name = @name" +
+          ", start_date = @start_date" +
+          ", end_date = @end_date" +
+          ", isactive = @isactive" +
+          ", last_edit_by = @last_edit_by" +
+          ", last_edit_date = GETDATE()" +
+          " WHERE topic_id = @topic_id AND id = @id"
+      );
+
+    console.log("updateSubTopic complete");
+    console.log("====================");
+    return { status: "ok", message: "อัพเดทข้อมูลในระบบเรียบร้อยแล้ว" };
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function getVersion() {
+  try {
+    return process.env.version;
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
 module.exports = {
   topicAttd: topicAttd,
   topicAttdDel: topicAttdDel,
   getTopicList: getTopicList,
+  getAllTopicList: getAllTopicList,
   getSubTopicList: getSubTopicList,
+  getAllSubTopicList: getAllSubTopicList,
   getAttd: getAttd,
   getAttdPsn: getAttdPsn,
   getAttdPsnByTopic: getAttdPsnByTopic,
   getExcelReport: getExcelReport,
-  getVersion: getVersion,
   voteCheck: voteCheck,
   vote: vote,
+  addTopic: addTopic,
+  updateTopic: updateTopic,
+  addSubTopic: addSubTopic,
+  updateSubTopic: updateSubTopic,
+  getVersion: getVersion,
 };
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const dboperations = require("./dboperations");
 
+const fs = require("fs");
 var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
+const multer = require("multer");
+const mkdirp = require("mkdirp");
+const sharp = require("sharp");
+const path = require("path");
+const { v4: uuid } = require("uuid");
 const { request, response } = require("express");
 var app = express();
 var router = express.Router();
+
+var config = require("./dbconfig");
+const sql = require("mssql");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -32,6 +41,62 @@ router.route("/health").get((request, response) => {
   // console.log("health check");
   response.json({ status: 200 });
 });
+
+let topicId = "";
+
+const UPLOAD_PATH = path.join(process.env.imgPath, "/TRS");
+mkdirp.sync(path.join(process.env.imgPath, "/TRS"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, done) => {
+    done(null, UPLOAD_PATH);
+  },
+  filename: (req, file, done) => {
+    done(null, uuid() + "___" + file.originalname);
+  },
+});
+
+const limits = {
+  fileSize: 5 * 1024 * 1024,
+};
+
+const fileFilter = (request, file, done) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    done(null, true);
+  } else {
+    done(new Error("file type not supported"), false);
+  }
+};
+
+const upload = multer({ storage, limits, fileFilter }).single("image");
+
+async function imgUpload(file, id) {
+  console.log("Image check");
+  if (!file) {
+    console.log("No image found to upload");
+  } else {
+    const newFilePath = path.join(UPLOAD_PATH, id + ".jpg");
+    // save newFilePath in your db as image path
+    await sharp(file.path).resize().jpeg({ quality: 50 }).toFile(newFilePath);
+    fs.unlinkSync(file.path);
+
+    console.log("Image upload complete");
+  }
+}
+
+async function genNewTopicId() {
+  console.log("generate new id");
+  let pool = await sql.connect(config);
+  const lastId = await pool
+    .request()
+    .query("SELECT TOP (1) id FROM trs_topic ORDER BY id DESC");
+  if (lastId.recordset.length !== 0) {
+    const newId = lastId.recordset[0].id.slice(3);
+    return `TRS${String(parseInt(newId) + 1).padStart(6, "0")}`;
+  } else {
+    return "TRS000001";
+  }
+}
 
 router.route("/topicattd").post((request, response) => {
   let attdData = { ...request.body };
@@ -76,9 +141,33 @@ router.route("/gettopiclist").get((request, response) => {
     });
 });
 
+router.route("/getalltopiclist").get((request, response) => {
+  dboperations
+    .getAllTopicList()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.sendStatus(500);
+    });
+});
+
 router.route("/getsubtopiclist").get((request, response) => {
   dboperations
     .getSubTopicList()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.sendStatus(500);
+    });
+});
+
+router.route("/getallsubtopiclist").get((request, response) => {
+  dboperations
+    .getAllSubTopicList()
     .then((result) => {
       response.json(result);
     })
@@ -136,16 +225,20 @@ router.route("/getexcelreport/:topic_id").get((request, response) => {
     });
 });
 
-router.route("/getversion").get((request, response) => {
-  dboperations
-    .getVersion()
-    .then((result) => {
-      response.json(result);
-    })
-    .catch((err) => {
+router.route("/getimg/:id").get((request, response) => {
+  try {
+    const readStream = fs.createReadStream(
+      `${process.env.imgPath}/TRS/${request.params.id}.jpg`
+    );
+    readStream.on("error", function (err) {
       console.error(err);
-      response.setStatus(500);
+      response.json({ status: "error", message: "No image found" });
     });
+    readStream.pipe(response);
+  } catch (error) {
+    console.error(error);
+    response.sendStatus(500);
+  }
 });
 
 router.route("/votecheck/:id").get((request, response) => {
@@ -170,6 +263,110 @@ router.route("/vote").post((request, response) => {
     .catch((err) => {
       console.error(err);
       response.sendStatus(500);
+    });
+});
+
+router.route("/addtopic").post(async (request, response) => {
+  topicId = await genNewTopicId();
+  console.log("new topic id = " + topicId);
+  upload(request, response, async (err) => {
+    if (err) {
+      return response
+        .status(400)
+        .json({ success: false, message: err.message });
+    }
+    try {
+      let img = 0;
+      const data = request.body;
+      const { file } = request;
+      if (file) {
+        img = 1;
+        await imgUpload(file, topicId);
+      }
+      dboperations
+        .addTopic(data, topicId, img)
+        .then((result) => {
+          response.json(result);
+        })
+        .catch((err) => {
+          console.error(err);
+          response.sendStatus(500);
+        });
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ success: false, message: error.message });
+    }
+  });
+});
+
+router.route("/updatetopic").post(async (request, response) => {
+  upload(request, response, async (err) => {
+    if (err) {
+      return response
+        .status(400)
+        .json({ success: false, message: err.message });
+    }
+    try {
+      const data = request.body;
+      const { file } = request;
+      let img = data.img;
+      if (file) {
+        img = 1;
+        await imgUpload(file, data.id);
+      }
+      dboperations
+        .updateTopic(data, img)
+        .then((result) => {
+          response.json(result);
+        })
+        .catch((err) => {
+          console.error(err);
+          response.sendStatus(500);
+        });
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ success: false, message: error.message });
+    }
+  });
+});
+
+router.route("/addsubtopic").post((request, response) => {
+  let data = { ...request.body };
+  dboperations
+    .addSubTopic(data)
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.sendStatus(500);
+    });
+});
+
+router.route("/updatesubtopic").post((request, response) => {
+  let data = { ...request.body };
+  dboperations
+    .updateSubTopic(data)
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.sendStatus(500);
+    });
+});
+
+router.route("/getversion").get((request, response) => {
+  dboperations
+    .getVersion()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.setStatus(500);
     });
 });
 
